d083a507f60decce13faad6de40e0043bfb87d25
Blaiva <blayverthreyesp14@gmail.com>
2025-09-06 15:33:11 -0400

    Creacion de los Pages: Index, Create, Edit, Delete. Modificacion de JugadoresService. Finalizacion del proyecto.

--- RegistroJugadores/Services/JugadoresService.cs
+++ RegistroJugadores/Services/JugadoresService.cs
@@ -13,6 +13,12 @@ namespace RegistroJugadores.Services
             return await contexto.Jugadores.AnyAsync(j => j.JugadorId == jugadorId);
         }
 
+        public async Task<bool> ExisteNombres(string nombres)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Jugadores.AnyAsync(j => j.Nombres.Equals(nombres));
+        }
+
         public async Task<bool> Insertar(Jugadores jugador)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
@@ -20,16 +26,9 @@ namespace RegistroJugadores.Services
             return await contexto.SaveChangesAsync() > 0;
         }
 
-        public async Task<bool> Modificar(Jugadores jugador)
-        {
-            await using var contexto = await DbFactory.CreateDbContextAsync();
-            contexto.Update(jugador);
-            return await contexto.SaveChangesAsync() > 0;
-        }
-
         public async Task<bool> Guardar(Jugadores jugador)
         {
-            if(!await Existe(jugador.JugadorId))
+            if(!await Existe(jugador.JugadorId) && !await ExisteNombres(jugador.Nombres))
             {
                 return await Insertar(jugador);
             }
@@ -39,10 +38,23 @@ namespace RegistroJugadores.Services
             }
         }
 
+        public async Task<bool> Modificar(Jugadores jugador)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Update(jugador);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+
         public async Task<bool> Eliminar(int jugadorId)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
             return await contexto.Jugadores.AsNoTracking().Where(j => j.JugadorId == jugadorId).ExecuteDeleteAsync() > 0;
+        }
+
+        public async Task<Jugadores?> Buscar(int jugadorId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Jugadores.FirstOrDefaultAsync(j=>j.JugadorId==jugadorId);
         }
 
         public async Task<List<Jugadores>> Listar(Expression<Func<Jugadores, bool>> criterio)
