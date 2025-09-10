39af2a92a070afb36c7a610d7569ba4f4bcb386b
Blaiva <blayverthreyesp14@gmail.com>
2025-09-10 15:32:35 -0400

    Modificacion del contexto, nueva migracion

--- TicTacToeBlaiva/DAL/Contexto.cs
+++ TicTacToeBlaiva/DAL/Contexto.cs
@@ -8,4 +8,35 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Jugadores> Jugadores { get; set; }
+
+    public DbSet<Partidas> Partidas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Partidas>(entity =>
+        {
+            entity.HasOne(p => p.Jugador1)
+                  .WithMany()
+                  .HasForeignKey(p => p.Jugador1Id)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(p => p.Jugador2)
+                  .WithMany()
+                  .HasForeignKey(p => p.Jugador2Id)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(p => p.Ganador)
+                  .WithMany()
+                  .HasForeignKey(p => p.GanadorId)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(p => p.TurnoJugador)
+                  .WithMany()
+                  .HasForeignKey(p => p.TurnoJugadorId)
+                  .OnDelete(DeleteBehavior.Restrict);
+        });
+    
+    }
 }
