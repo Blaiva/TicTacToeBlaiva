a20df5abd8d6adf47ceb8cd6b2bb1efe8822d60f
Blaiva <blayverthreyesp14@gmail.com>
2025-10-25 11:50:35 -0400

    Creacion de PartidasApiService y su interfaz, Creacion de TicTacToeIndex

---
+++ TicTacToeBlaiva.BlazorWasm/Services/IPartidasApiService.cs
@@ -0,0 +1,11 @@
+﻿using TicTacToeBlaiva.Shared;
+using TicTacToeBlaiva.Shared.DTOs;
+
+namespace TicTacToeBlaiva.BlazorWasm.Services;
+
+public interface IPartidasApiService
+{
+    Task<Resource<List<PartidaResponse>>> GetPartidasAsync();
+    Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId);
+    Task<Resource<PartidaResponse>> PostPartida(int jugador1, int jugador2);
+}
