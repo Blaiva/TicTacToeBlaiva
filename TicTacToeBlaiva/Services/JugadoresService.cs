aa9faa2fef2cbca607ac6ec94b98f3961bfe070f
Blaiva <blayverthreyesp14@gmail.com>
2025-10-25 11:04:46 -0400

    Creacion del Proyecto Web Assembly

--- TicTacToeBlaiva/Services/JugadoresService.cs
+++ TicTacToeBlaiva/Services/JugadoresService.cs
@@ -8,7 +8,7 @@ public class JugadoresService(IDbContextFactory<Contexto> DbFactory)
 {
     public async Task<bool> Existe(int jugadorId)
     {
-        await using var contexto = await DbFactory.CreateDbContextAsync();
+        using Contexto contexto = await DbFactory.CreateDbContextAsync();
         return await contexto.Jugadores.AnyAsync(j => j.JugadorId == jugadorId);
     }
 
