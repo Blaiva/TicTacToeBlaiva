f4b56538ce6aff38a3048fa16d3f6643a4117f8a
Blaiva <blayverthreyesp14@gmail.com>
2025-10-25 11:35:31 -0400

    Modificacion del Program en el Wasm y creacion de Resource y los DTOs en Shared

---
+++ TicTacToeBlaiva.Shared/DTOs/MovimientoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeBlaiva.Shared.DTOs;
+
+public record MovimientoRequest(
+    int PartidaId,
+    int JugadorId
+);
