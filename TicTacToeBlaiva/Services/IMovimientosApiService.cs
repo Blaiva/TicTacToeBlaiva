46c09c4801ff8067198e07a4497dd98dbeca2cdf
Blaiva <blayverthreyesp14@gmail.com>
2025-10-26 19:21:31 -0400

    Arreglo en MovimientoApiService

--- TicTacToeBlaiva/Services/IMovimientosApiService.cs
+++ TicTacToeBlaiva/Services/IMovimientosApiService.cs
@@ -5,6 +5,6 @@ namespace TicTacToeBlaiva.Services;
 
 public interface IMovimientosApiService
 {
-    Task<Resource<MovimientoResponse>> GetMovimientoAsync(int partidaId);
+    Task<Resource<List<MovimientoResponse>>> GetMovimientosAsync(int partidaId);
     Task<Resource<MovimientoResponse>> PostMovimiento(int partidaId, string jugador, int posicionFila, int posicionColumna);
 }
