7ff7c9067d02a92e04910fc02336ef03e327c376
Blaiva <blayverthreyesp14@gmail.com>
2025-10-29 09:48:19 -0400

    Modificacion de JugadoresIndex y Inyeccion de JugadoresApiService y su interfaz

--- TicTacToeBlaiva/Program.cs
+++ TicTacToeBlaiva/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddRazorComponents()
 var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 
+builder.Services.AddHttpClient<IJugadoresApiService, JugadoresApiService>(client =>
+{
+    client.BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/");
+});
+
 //Inyección del Service
 builder.Services.AddScoped<JugadoresService>();
 builder.Services.AddScoped<PartidasService>();
