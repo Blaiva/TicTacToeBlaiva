c104a194614a6594e01677d3968edf2c9771d451
Blaiva <blayverthreyesp14@gmail.com>
2025-11-01 17:32:47 -0400

    Fin del Branch

--- TicTacToeBlaiva/Services/IPartidasApiService.cs
+++ TicTacToeBlaiva/Services/IPartidasApiService.cs
@@ -7,4 +7,5 @@ public interface IPartidasApiService
     Task<Resource<List<PartidaResponse>>> GetPartidasAsync();
     Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId);
     Task<Resource<PartidaResponse>> PostPartida(int jugador1, int? jugador2);
+    Task<Resource<PartidaResponse>> PutPartida(int partidaId, int jugador1, int? jugador2);
 }
--- TicTacToeBlaiva/Services/PartidasApiService.cs
+++ TicTacToeBlaiva/Services/PartidasApiService.cs
@@ -49,4 +49,23 @@ public class PartidasApiService(HttpClient httpClient) : IPartidasApiService
             return new Resource<PartidaResponse>.Error("Respuesta inválida del servidor.");
         }
     }
+
+    public async Task<Resource<PartidaResponse>> PutPartida(int partidaId, int jugador1, int? jugador2)
+    {
+        var request = new PartidaRequest(jugador1, jugador2);
+        try
+        {
+            var response = await httpClient.PostAsJsonAsync($"api/Partidas/{partidaId}", request);
+            response.EnsureSuccessStatusCode();
+            return new Resource<PartidaResponse>.Success(null!);
+        }
+        catch (HttpRequestException ex)
+        {
+            return new Resource<PartidaResponse>.Error($"Error de red: {ex.Message}");
+        }
+        catch (NotSupportedException)
+        {
+            return new Resource<PartidaResponse>.Error("Respuesta inválida del servidor.");
+        }
+    }
 }
